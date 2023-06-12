1173adfc9c0ad5ad02ca3bfa615bcda2a1be81fc
lazarantonijevic <lazar.antonijevic@gmail.com>
2023-06-12 04:26:27 +0200

    CoinItem komponenta

---
+++ readme.txt
@@ -0,0 +1,9 @@
+-dodatne biblioteke
+
+react-icons
+react-router-dom
+axios
+
+
+-API
+https://www.coingecko.com/en/api/documentation
--- src/App.js
+++ src/App.js
@@ -1,12 +1,34 @@
 
 import Navbar from './components/Navbar';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import CoinItem from './components/CoinItem';
+import axios from 'axios';
 
+import React, { useState, useEffect } from 'react';
 
 function App() {
+
+  const [coin, setCoinItem] = useState([]);
+  const url = 'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&ids=bitcoin&order=market_cap_desc&per_page=30&page=1&sparkline=false';
+
+  useEffect(() => {
+    axios
+      .get(url)
+      .then((res) => {
+        setCoinItem(res.data);
+        console.log(res.data[0]);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+
   return (
     <BrowserRouter>
-      <Navbar />  
+      <Navbar /> 
+      <Routes>
+        <Route path='/' element={<CoinItem coin={coin} />} />
+      </Routes> 
     </BrowserRouter>  
   );
 }
